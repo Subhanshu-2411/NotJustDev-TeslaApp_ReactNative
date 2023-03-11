8a04a412df3a8419f7318036a19ad87751aaa20c
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-03-11 13:59:53 +0530

    Added FlatList Menu Options

--- components/MenuOption.js
+++ components/MenuOption.js
@@ -1,24 +1,23 @@
-import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import {StyleSheet, Text, Pressable} from "react-native";
 import {
-    FontAwesome,
-    Entypo,
     MaterialCommunityIcons,
-    FontAwesome5,
-    Ionicons,
     MaterialIcons,
 } from '@expo/vector-icons';
+import {Link} from "expo-router";
 export const MenuOption = ({item}) => {
     return (
-        <View style={styles.optionRow}>
-            <MaterialCommunityIcons name={item.iconName} size={26} color="gray" />
-            <Text style={styles.optionText}>{item.name}</Text>
-            <MaterialIcons
-                name="keyboard-arrow-right"
-                size={24}
-                color="gray"
-                style={{ marginLeft: 'auto' }}
-            />
-        </View>
+        <Link href={item.href} asChild>
+            <Pressable style={styles.optionRow}>
+                <MaterialCommunityIcons name={item.iconName} size={26} color="gray" />
+                <Text style={styles.optionText}>{item.name}</Text>
+                <MaterialIcons
+                    name="keyboard-arrow-right"
+                    size={24}
+                    color="gray"
+                    style={{ marginLeft: 'auto' }}
+                />
+            </Pressable>
+        </Link>
     )
 }
 
